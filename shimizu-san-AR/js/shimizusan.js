6acca9bdf1c936e3257e0123443170d8b80e2578
mizuki-kisa <kisaichi07@gmail.com>
2020-02-01 19:04:49 +0900

    アニメーション

--- shimizu-san-AR/js/shimizusan.js
+++ shimizu-san-AR/js/shimizusan.js
@@ -1,5 +1,5 @@
 const list = ["aaa","bbb","ccc","ddd","eee","fff","ggg"];
-//const imgList = ["01.png","02.png","03.png"];
+const imgList = ["01.png","02.png","03.png"];
 const objList = ["#crate-obj1","#crate-obj2"];
 const mtlList = ["#crate-mtl1","#crate-mtl2"];
 var count = 0;
@@ -9,6 +9,7 @@ console.log(list.length);
 window.onload = function() {
   serifu = document.getElementById("serifu");
   img = document.getElementById("model");
+  png = document.getElementById("png");
   serifu.addEventListener("click",changeSerifu);
   changeSerifu();
 
@@ -16,10 +17,11 @@ window.onload = function() {
 window.setInterval(animation, 500);
 
 function animation(){
-  //var currentImg = 'img/'+ imgList[count%imgList.length];
+  var currentImg = 'img/'+ imgList[count%imgList.length];
   //var currentObj = 'obj/'+ modelList[count%modelList.length];
+  png.setAttribute('src', currentImg);
   img.setAttribute('src', objList[count%objList.length]);
-  img.setAttribute('mtl', mtlList[count%mtlList.length]);
+  //img.setAttribute('mtl', mtlList[count%mtlList.length]);
   count ++ ;
 }
 
